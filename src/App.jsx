4f1212fb35c4b7b1fd945e8ca1f5cfa1032ec8a5
emazupp <zuppardo.emanuele2001@gmail.com>
2024-12-02 18:30:30 +0100

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,6 @@ import Header from "./components/layout/Header";
 import { languages } from "./data/languages";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <Header />
